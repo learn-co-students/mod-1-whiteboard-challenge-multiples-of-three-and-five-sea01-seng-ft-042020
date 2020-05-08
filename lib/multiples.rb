828b0e9d1e56a9a9dd0f0002a4eb5bc2969ad5f7
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-07 21:03:59 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
-# Enter your procedural solution here!
+require 'pry'
+def collect_multiples(limit)
+    (3...limit).to_a.filter{|i| i % 3 == 0 or i % 5 == 0}
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,14 @@
-# Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+    def initialize(limit)
+        self.limit = limit
+    end
+
+    def collect_multiples
+        (3...self.limit).to_a.filter{|i| i % 3 == 0 or i % 5 == 0}
+    end
+    
+    def sum_multiples
+        self.collect_multiples.sum
+    end
+end
